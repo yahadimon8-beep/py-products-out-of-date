fc9a0143a9b57e1cdf3ee21135f6baa02eb7b18c
yahadimon8-deep <yahadimon8@gmail.com>
2026-09-12 17:38:54 +0300

    solution: app/test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,11 @@
-# write your code here
+import pytest
+from unittest.mock import patch
+from datetime import date
+from app.main import outdated_products
+
+
+@patch("datetime.date")
+def test_outdated_products(mock_today: pytest.Mock) -> None:
+    mock_today.return_value = date(2022, 2, 15)
+    results = outdated_products([])
+    assert results == []
